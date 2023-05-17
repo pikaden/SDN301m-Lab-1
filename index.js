96e2af22399a243d2c8b817e4e6e0e5ea52d2ad6
pikaden <87664605+pikaden@users.noreply.github.com>
2023-05-17 23:38:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express'),
+    http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
